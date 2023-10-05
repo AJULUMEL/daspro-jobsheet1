6c524733cf21f706c720f12ddaf44bf9cb8caa95
AJULUMEL <azruljoss@gmail.com>
2023-10-05 01:11:08 -0700

    first commit

---
+++ Latihan06.java
@@ -0,0 +1,11 @@
+/**
+ * Latihan06
+ */
+public class Latihan06 {
+
+    public static void main(String[] args){
+System.out.print("Nama saya Dandi Azrul Syahputra") ;
+//System.out.print("\nsaya adalah mahasiswa prodi teknik informatika");   
+ }
+}
+
---
+++ MyFirstJava06.java
@@ -0,0 +1,10 @@
+public class MyFirstJava06 {
+    public static void main(String[] args) {
+        System.out.println("Nama\tDandi Azrul Syahputra");
+        System.out.println("Absen\t06");
+        System.out.println("Kelas\t1D");
+        System.out.println("Prodi\tD-IV Teknik Informatika");
+        System.out.println("Jurusan\tTeknologi Informasi");
+    
+    }    
+}
